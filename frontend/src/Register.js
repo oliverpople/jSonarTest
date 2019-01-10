ca097bacb0848fc92d7c69b2762af88aacd475ba
oliverpople <oliverpople@gmail.com>
2019-01-09 17:45:03 -0800

    Added event handler to Register component on receiving code 200 from backend user is reverted back to login page. Next will add Loginscreen component to toggle between login and register components. The context of Loginscreen.js page is sent as the  parentContext prop which allows us to manipulate state variables of loginscreen from it`s children.

--- frontend/src/Register.js
+++ frontend/src/Register.js
@@ -4,6 +4,7 @@ import AppBar from "material-ui/AppBar";
 import RaisedButton from "material-ui/RaisedButton";
 import TextField from "material-ui/TextField";
 import axios from "axios";
+import Login from "./Login";
 
 const style = {
   margin: 15
@@ -18,6 +19,46 @@ class Register extends Component {
     };
   }
 
+  handleClick(event) {
+    var apiBaseUrl = "http://localhost:4000/api/";
+    var self = this;
+    if (this.state.username.length > 0 && this.state.password.length > 0) {
+      var payload = {
+        username: this.state.username,
+        password: this.state.password
+      };
+      axios
+        .post(apiBaseUrl + "/register", payload)
+        .then(function(response) {
+          console.log(response);
+          if (response.data.code === 200) {
+            //  console.log("registration successfull");
+            var loginscreen = [];
+            loginscreen.push(
+              <Login
+                key="login-component"
+                parentContext={this}
+                appContext={self.props.appContext}
+              />
+            );
+            var loginmessage = "Not Registered yet. Go to registration";
+            self.props.parentContext.setState({
+              loginscreen: loginscreen,
+              loginmessage: loginmessage,
+              buttonLabel: "Register",
+              isLogin: true
+            });
+          } else {
+            console.log("some error ocurred", response.data.code);
+          }
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    } else {
+      alert("Input field value is missing");
+    }
+  }
   render() {
     return (
       <div>

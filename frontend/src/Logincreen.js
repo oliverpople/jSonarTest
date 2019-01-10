2f231289132db1db1944ca61912df2189a5a987d
oliverpople <oliverpople@gmail.com>
2019-01-09 18:20:57 -0800

    Added event handlers for handling user authentication permission.

--- frontend/src/Logincreen.js
+++ frontend/src/Logincreen.js
@@ -56,6 +56,81 @@ class Loginscreen extends Component {
     });
   }
 
+  handleClick(event) {
+    console.log("event");
+    if (this.state.isLogin) {
+      this.isLoggedIn(event);
+    } else {
+      this.notLoggedIn(event);
+    }
+  }
+
+  isLoggedIn(event) {
+    let loginscreen = [];
+    loginscreen.push(
+      <Register
+        key="register-component"
+        parentContext={this}
+        appContext={this.props.appContext}
+      />
+    );
+    var loginmessage = "Already registered? Go to Login";
+    let loginButtons = [];
+    loginButtons.push(
+      <div key="login-button">
+        <MuiThemeProvider>
+          <div>
+            <RaisedButton
+              label={"Login"}
+              primary={true}
+              style={style}
+              onClick={event => this.handleClick(event)}
+            />
+          </div>
+        </MuiThemeProvider>
+      </div>
+    );
+    this.setState({
+      loginscreen: loginscreen,
+      loginmessage: loginmessage,
+      loginButtons: loginButtons,
+      isLogin: false
+    });
+  }
+
+  notLoggedIn(event) {
+    let loginscreen = [];
+    let loginButtons = [];
+    loginButtons.push(
+      <div key="register-button">
+        <MuiThemeProvider>
+          <div>
+            <RaisedButton
+              label={"Register"}
+              primary={true}
+              style={style}
+              onClick={event => this.handleClick(event, "register")}
+            />
+          </div>
+        </MuiThemeProvider>
+      </div>
+    );
+    loginscreen.push(
+      <Login
+        key="login-component"
+        parentContext={this}
+        appContext={this.props.appContext}
+      />
+    );
+    var loginmessage = "Not Registered yet? Go to registration";
+    this.setState({
+      loginscreen: loginscreen,
+      loginmessage: loginmessage,
+      loginButtons: loginButtons,
+      isLogin: true
+    });
+  }
+
   render() {
     return (
       <div className="loginscreen" key="loginscreen">

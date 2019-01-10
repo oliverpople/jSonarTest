24f10e89ead6fb1e9c5e4ad258bde3d51de52cd2
oliverpople <oliverpople@gmail.com>
2019-01-09 18:27:23 -0800

    Set App component to render either login page or data page based on the event handling that occurs within Loginscreen component.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,31 @@
 import React, { Component } from "react";
 import "./App.css";
+import LoginScreen from "./Loginscreen";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loginPage: [],
+      dataScreen: []
+    };
+  }
+
+  componentWillMount() {
+    var loginPage = [];
+    loginPage.push(<LoginScreen key="login-screen" appContext={this} />);
+    this.setState({
+      loginPage: loginPage
+    });
+  }
+
   render() {
-    return <div className="App">Learn React</div>;
+    return (
+      <div className="App">
+        {this.state.loginPage}
+        {this.state.dataScreen}
+      </div>
+    );
   }
 }
 

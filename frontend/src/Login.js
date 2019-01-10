359d73f2c3d832eeeac0295478c3dd9b27c007f0
oliverpople <oliverpople@gmail.com>
2019-01-09 17:27:30 -0800

    Added login component to handle username and password fields.

---
+++ frontend/src/Login.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import AppBar from "material-ui/AppBar";
+import RaisedButton from "material-ui/RaisedButton";
+import TextField from "material-ui/TextField";
+var apiBaseUrl = "http://localhost:4000/api/";
+import axios from "axios";
+
+const style = {
+  margin: 15
+};
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <MuiThemeProvider>
+          <AppBar title="Login" />
+        </MuiThemeProvider>
+        <MuiThemeProvider key="local-login-Component">
+          <div>
+            <TextField
+              hintText="Enter your username"
+              floatingLabelText="username"
+              onChange={(event, newValue) =>
+                this.setState({ username: newValue })
+              }
+            />
+            <br />
+            <TextField
+              type="password"
+              hintText="Enter your Password"
+              floatingLabelText="Password"
+              onChange={(event, newValue) =>
+                this.setState({ password: newValue })
+              }
+            />
+            <br />
+            <RaisedButton
+              label="Submit"
+              primary={true}
+              style={style}
+              onClick={event => this.handleClick(event)}
+            />
+          </div>
+        </MuiThemeProvider>
+      </div>
+    );
+  }
+}
+
+export default Login;

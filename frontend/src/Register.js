267d7292e099c04c4ec3532156b380294da0015a
oliverpople <oliverpople@gmail.com>
2019-01-09 17:38:43 -0800

    Added Register component for handling user registeration detail submissions.

---
+++ frontend/src/Register.js
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import AppBar from "material-ui/AppBar";
+import RaisedButton from "material-ui/RaisedButton";
+import TextField from "material-ui/TextField";
+import axios from "axios";
+
+const style = {
+  margin: 15
+};
+
+class Register extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <MuiThemeProvider>
+          <div>
+            <AppBar title="Register" />
+            <TextField
+              hintText="Enter your username"
+              floatingLabelText="username"
+              onChange={(event, newValue) =>
+                this.setState({ username: newValue })
+              }
+            />
+            <br />
+            <TextField
+              type="password"
+              hintText="Enter your Password"
+              floatingLabelText="Password"
+              onChange={(event, newValue) =>
+                this.setState({ password: newValue })
+              }
+            />
+            <br />
+            <RaisedButton
+              label="Submit"
+              primary={true}
+              style={style}
+              onClick={event => this.handleClick(event)}
+            />
+          </div>
+        </MuiThemeProvider>
+      </div>
+    );
+  }
+}
+
+export default Register;

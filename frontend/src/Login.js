42b9d634414f3e59dde7846f8d6de374f4b9c5c6
oliverpople <oliverpople@gmail.com>
2019-01-09 17:31:19 -0800

    Added event handler to Login component. Handler post user log in data to server api and has error handling. A successful post request brings the DataDiscoveryPage into view.

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -5,6 +5,7 @@ import RaisedButton from "material-ui/RaisedButton";
 import TextField from "material-ui/TextField";
 var apiBaseUrl = "http://localhost:4000/api/";
 import axios from "axios";
+import DataDiscoveryPage from "./DataDiscoveryPage";
 
 const style = {
   margin: 15
@@ -19,6 +20,41 @@ class Login extends Component {
     };
   }
 
+  handleClick(event) {
+    var self = this;
+    var payload = {
+      username: this.state.username,
+      password: this.state.password
+    };
+    axios
+      .post(apiBaseUrl + "login", payload)
+      .then(function(response) {
+        console.log(response);
+        if (response.data.code === 200) {
+          console.log("Login successfull");
+          var dataScreen = [];
+          dataScreen.push(
+            <DataDiscoveryPage
+              key="Data-Discovery-Page"
+              appContext={self.props.appContext}
+            />
+          );
+          self.props.appContext.setState({
+            loginPage: [],
+            dataScreen: dataScreen
+          });
+        } else if (response.data.code === 204) {
+          console.log("Username password do not match");
+          alert(response.data.success);
+        } else {
+          console.log("Username does not exists");
+          alert("Username does not exist");
+        }
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
   render() {
     return (
       <div>

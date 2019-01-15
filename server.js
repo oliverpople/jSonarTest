bac8236df6e0c9217ec8891dfee393f8c6b25146
oliverpople <oliverpople@gmail.com>
2019-01-15 14:30:21 -0800

    amending server, adding procfile.

--- server.js
+++ server.js
@@ -32,4 +32,14 @@ router.get("/customernames", customersRoutes.customernames);
 router.post("/customerfilter", customersRoutes.customerfilter);
 router.post("/customerorderinfo", customersRoutes.customerorderinfo);
 app.use("/api", router);
-app.listen(4000);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT);

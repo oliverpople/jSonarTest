a97f8155498dd9d5b925d9353db984b2c0ad7bcd
oliverpople <oliverpople@gmail.com>
2019-01-17 11:22:21 -0800

    Amending file path.

--- server.js
+++ server.js
@@ -30,8 +30,8 @@ app.use("/api", router);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   const path = require("path");
-  app.get("/*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
   });
 }
 

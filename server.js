14dd8426a4069aefa006521af4a1b2e839c39866
oliverpople <oliverpople@gmail.com>
2019-04-01 15:35:11 -0700

    Added cors npm package to express server.

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ var express = require("express");
 var login = require("./routes/loginroutes");
 var customersRoutes = require("./routes/customersRoutes");
 var bodyParser = require("body-parser");
+var cors = require("cors");
 var app = express();
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 

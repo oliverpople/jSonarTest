6666948a62a3b91fef4e52eff124e3716d54825b
oliverpople <oliverpople@gmail.com>
2019-04-01 10:33:13 -0700

    Server now connects to remote customer data mySQL db on Cleardb.

--- backend/routes/customersRoutes.js
+++ backend/routes/customersRoutes.js
@@ -1,19 +1,44 @@
 require("dotenv").config();
 var mysql = require("mysql");
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
+var db_config = {
+  host: "us-cdbr-iron-east-01.cleardb.net",
+  user: "b168a3716d34ba",
   password: process.env.DB_PASSWORD,
-  database: "classicmodels"
-});
+  database: "heroku_9914b8101fa5760"
+};
 
-connection.connect(function(err) {
-  if (!err) {
-    console.log("classicmodels database is connected ... nn");
-  } else {
-    console.log("Error connecting to classicmodels database ... nn", err);
-  }
-});
+var connection;
+
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config); // Recreate the connection, since
+  // the old one cannot be reused.
+
+  connection.connect(function(err) {
+    if (!err) {
+      console.log(
+        "heroku_9914b8101fa5760 (classicmodels) database is connected ... nn"
+      );
+      // The server is either down
+    } else {
+      // or restarting (takes a while sometimes).
+      console.log("error when connecting to db:", err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    } // to avoid a hot loop, and to allow our node script to
+  }); // process asynchronous requests in the meantime.
+  // If you're also serving http, display a 503 error.
+  connection.on("error", function(err) {
+    console.log("db error", err);
+    if (err.code === "PROTOCOL_CONNECTION_LOST") {
+      // Connection to the MySQL server is usually
+      handleDisconnect(); // lost due to either server restart, or a
+    } else {
+      // connnection idle timeout (the wait_timeout
+      throw err; // server variable configures this)
+    }
+  });
+}
+
+handleDisconnect();
 
 exports.customernames = function(req, res) {
   connection.query(
